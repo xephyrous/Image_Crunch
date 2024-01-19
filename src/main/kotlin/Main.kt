06d947e5d4b59c237919d2fddbf0b28e3ae3bc46
aidanmao2005 <aidanmao2005@gmail.com>
2024-01-19 13:39:58 -0500

    Image Selection
    = feat: Added File Picker
    = feat: Added SnackBar for App()
    
    = todo: Implement Image Display
    = todo: Finish UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,54 +1,91 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.sharp.Add
 import androidx.compose.material.icons.sharp.Build
 import androidx.compose.material.icons.sharp.Edit
 import androidx.compose.material.icons.sharp.List
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.launch
 import utils.GenerateNodes
 import utils.NodeGeneratorType
+import utils.ImageFileSelection
+import javax.imageio.ImageIO
+import java.io.File
 
 @Composable
 @Preview
 fun App() {
     var genBtnState by mutableStateOf(false)
-
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
     Scaffold(
+        snackbarHost = {
+                       SnackbarHost(hostState = snackbarHostState)
+        },
         topBar = {
             TopAppBar (
                 title = {
                     Text("Image Crunch α")
                 },
 
-                actions = {
-                    IconButton(onClick = { /* Do shit */}) {
+                actions = { //TODO: Add image to main panel
+                    IconButton(onClick = {
+                        selectedImage = ImageFileSelection()
+
+                        if (selectedImage == null) {
+                            scope.launch {
+                                snackbarHostState.showSnackbar("Failed to Load")
+                            }
+                        } else {
+                            scope.launch {
+                                snackbarHostState.showSnackbar("Image Loaded")
+                            }
+                        }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Sharp.Add,
+                            contentDescription = "Select Image"
+                        )
+                    }
+                    
+                    IconButton(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Settings Clicked")
+                        }
+                    }) {
                         Icon(
                             imageVector = Icons.Sharp.Build,
                             contentDescription = "Settings"
                         )
                     }
 
-                    IconButton(onClick = { /* Do shit */}) {
+                    IconButton(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Edit Pressed")
+                        }
+                    }) {
                         Icon(
                             imageVector = Icons.Sharp.Edit,
                             contentDescription = "Test Text"
                         )
                     }
 
-                    IconButton(onClick = { /* Do shit */}) {
+                    IconButton(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("List Pressed")
+                        }
+                    }) {
                         Icon(
                             imageVector = Icons.Sharp.List,
                             contentDescription = "Test Text"
@@ -60,6 +97,8 @@ fun App() {
     ) {
     }
 }
+
+var selectedImage: File? = null
 
 fun main() = application {
     val state = rememberWindowState(
---
+++ src/main/kotlin/utils/file_selection.kt
@@ -0,0 +1,32 @@
+package utils
+
+import java.io.File
+import java.io.FileFilter
+import java.util.*
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
+
+// i hope this works
+fun ImageFileSelection(): File? {
+    val fileFilter = FileNameExtensionFilter("Image Files", "jpg", "png", "jpeg")
+    val fileChooser = JFileChooser()
+    fileChooser.fileFilter = fileFilter
+
+    val sourceReturn = fileChooser.showOpenDialog(null)
+
+    if (sourceReturn == JFileChooser.APPROVE_OPTION) {
+        return fileChooser.selectedFile
+    }
+    return null
+}
+
+private fun getExtension(input: File): String? {
+    var extension: String? = null
+    val fileName = input.name
+    val x = fileName.lastIndexOf('.')
+
+    if (x > 0 && x < (fileName.length - 1)) {
+        extension = fileName.substring(x+1).lowercase()
+    }
+    return extension
+}
