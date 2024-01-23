8835fd730db3b65f59c157d10f37589c477eaa76
aidanmao2005 <aidanmao2005@gmail.com>
2024-01-23 11:11:09 -0500

    Some UI changes and stuff = feat: setup some "backend" for settings (in quotations) = feat: DISPLAYS IMAGES (doesn't scale yet) = feat: had a mental breakdown in comments = feat: added settings_export.kt (does nothing)
    = change: menus move now
    
    = todo: add settings
    = todo: scale images when displayed
    = todo: figure out how to scale everything else

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,67 @@
+import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.loadImageBitmap
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import kotlinx.coroutines.launch
-import utils.GenerateNodes
-import utils.NodeGeneratorType
-import utils.ImageFileSelection
+import utils.*
 import java.io.File
+import java.io.FileInputStream
 
 @Composable
 @Preview
 fun App() {
-    var genBtnState by mutableStateOf(false)
+    // Settings
+    var compactExport by remember { mutableStateOf(true) }
+    var genBtnState by remember { mutableStateOf(false) }
+
+    // Testing Stuff
+    var caption by remember { mutableStateOf("theres NO PICTURE") }
+
+    // holy mess
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
-    var cardState by remember { mutableStateOf(false) }
+    var displayed by remember { mutableStateOf(false) }
+
+    // moves the cards on so its more responsive (i guess)
+    var menuCardState by remember { mutableStateOf(false) }
+    var settingsCardState by remember { mutableStateOf(false) }
+    val offset by animateIntOffsetAsState(
+        targetValue = if (menuCardState) {
+            // Temporary maybe? moves menu menu past settings menu if both are open
+            if (!settingsCardState) {
+                IntOffset(10, 50)
+            } else {
+                IntOffset(610, 50)
+            }
+        } else {
+            IntOffset(-700, 50)
+        },
+        label = "offset"
+    )
+
+    // change this if u dare
+    val owoffset by animateIntOffsetAsState(
+        targetValue = if (settingsCardState) {
+            IntOffset(10, 50)
+        } else {
+            IntOffset(-700, 50)
+        },
+        label = "owoffset"
+    )
+
     Scaffold(
         snackbarHost = {
                        SnackbarHost(hostState = snackbarHostState)
@@ -42,17 +73,23 @@ fun App() {
                 },
 
                 actions = { //TODO: Add image to main panel
+                    // holy spaghetti
                     IconButton(onClick = {
-                        selectedImage = ImageFileSelection()
+                        if (!displayed) {
+                            selectedImage = ImageFileSelection()
+                        } else {
+                            val tempImage = ImageFileSelection()
+                            if (tempImage != null) {
+                                selectedImage = tempImage
+                            }
+                        }
 
                         if (selectedImage == null) {
-                            scope.launch {
-                                snackbarHostState.showSnackbar("Failed to Load")
-                            }
+                            caption = "theres NO PICTURE"
+                            displayed = false
                         } else {
-                            scope.launch {
-                                snackbarHostState.showSnackbar("Image Loaded")
-                            }
+                            caption = "omg it worked"
+                            displayed = true
                         }
                     }) {
                         Icon(
@@ -62,9 +99,7 @@ fun App() {
                     }
                     
                     IconButton(onClick = {
-                        scope.launch {
-                            snackbarHostState.showSnackbar("Settings Clicked")
-                        }
+                        settingsCardState = !settingsCardState
                     }) {
                         Icon(
                             imageVector = Icons.Sharp.Build,
@@ -72,7 +107,7 @@ fun App() {
                         )
                     }
                     IconButton(onClick = {
-                        cardState = true
+                        menuCardState = !menuCardState
                     }) {
                         Icon(
                             imageVector = Icons.Sharp.List,
@@ -94,21 +129,69 @@ fun App() {
             modifier = Modifier.padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ){
-//            Image(
-//                loadImageBitmap(inputStream = FileInputStream(selectedImage)),
-//                "temp"
-//            )
-            Text("temp")
-        }
-        if(cardState) {
-            Card(
-                modifier = Modifier.size(300.dp, 600.dp),
-                elevation = 10.dp
-
-            ) {
-                Text(text = "test")
+            if(displayed) {
+                Image(
+                    loadImageBitmap(inputStream = FileInputStream(selectedImage!!)),
+                    "temp"
+                )
             }
+            Text(caption)
         }
+        Card(
+            modifier = Modifier
+                // how do i scale the size of this?!?!!??!
+                .size(300.dp, 600.dp)
+                .offset { offset },
+            elevation = 20.dp
+        ) {
+            // TODO: make this actual stuff in the menu
+            // Basic Menu
+            Row() {
+                // TODO: make this switch animate the other menu stuff moving down and the option fading in (suffer)
+                Switch(
+                    checked = compactExport,
+                    onCheckedChange = {
+                        compactExport = it
+                    }
+                )
+                Text("Compact Settings Export")
+                Button(
+                    onClick = {
+                        if (compactExport) {
+                            settingsToString()
+                        } else {
+                            settingsToCSV()
+                        }
+                    },
+                    modifier = Modifier.offset { IntOffset(5, 0) }
+                    ) {
+                    Text("Export")
+                }
+            }
+        }// End of the card
+
+        Card(
+            modifier = Modifier
+                // how do i scale the size of this?!?!!??!
+                .size(300.dp, 600.dp)
+                .offset { owoffset },
+            elevation = 20.dp
+        ) {
+            // TODO: WHAT THE FLUFF IS A SETTINGS
+            // settings menu????!?!!!
+            Row() {
+                Text(text = "this is supposed to be a settings menu :O")
+
+                IconButton(onClick = {
+                    // There is no settings
+                }) {
+                    Icon(
+                        imageVector = Icons.Sharp.AddCircle,
+                        contentDescription = "Test Text"
+                    )
+                }
+            }
+        }// End of the card 2.0
     }
 }
 
@@ -117,7 +200,7 @@ var selectedImage: File? = null
 fun main() = application {
     val state = rememberWindowState(
         size = DpSize(1200.dp, 800.dp),
-        position = WindowPosition(300.dp, 300.dp)
+        position = WindowPosition(150.dp, 150.dp)
     )
 
     Window(title = "Image Crunch v0.1.a", onCloseRequest = ::exitApplication, state = state) {
---
+++ src/main/kotlin/utils/settings_export.kt
@@ -0,0 +1,12 @@
+package utils
+
+// empty until there are actual settings to export
+
+fun settingsToCSV() {
+    // There's nothing to make...
+}
+
+fun settingsToString(): String {
+    // Still nothing to make
+    return ""
+}
