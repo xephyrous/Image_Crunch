4263be01cbee8d03b0d3da592fcfde48f3a938c0
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2024-02-01 16:55:31 -0500

    Node Mask now customizeable by caller

--- src/main/kotlin/utils/image_overlay.kt
+++ src/main/kotlin/utils/image_overlay.kt
@@ -1,45 +1,26 @@
 package utils
 
+import java.awt.BasicStroke
 import java.awt.Color
+import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 
 // Copies image and displays a mask, shoutout to O(nxm)
-fun displayNodeMask(input: BufferedImage, nodes: ArrayList<PositionNode>): BufferedImage {
+fun createNodeMask(
+    input: BufferedImage,
+    nodes: ArrayList<PositionNode>,
+    nodeColor: Color = Color.BLACK,
+    nodeSize: Int = 3,
+): BufferedImage {
     // Create Output
     val img = BufferedImage(loadedImageSize.width, loadedImageSize.height, BufferedImage.TYPE_INT_ARGB)
+    val gpx: Graphics2D = img.graphics as Graphics2D
 
-    try {
-        for (node in nodes) {
-            try {
-                img.setRGB(node.second, node.first, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second+1, node.first, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second, node.first+1, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second+1, node.first+1, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second+1, node.first-1, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second-1, node.first+1, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second-1, node.first, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second, node.first-1, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-            try {
-                img.setRGB(node.second-1, node.first-1, Color(0,0,0).rgb)
-            } catch (_: Exception) {}
-        }
-    } catch(e: Exception) {
-        println(e.stackTrace)
+    gpx.color = nodeColor
+    gpx.stroke = BasicStroke(5f)
+
+    for(node in nodes) {
+        gpx.fillOval(node.second, node.first, nodeSize, nodeSize)
     }
 
     return img
