038bc2eca172c84b0f49b0c4722a674faf0ab2dc
AlexanderYauchler <135302972+terminatedNULL@users.noreply.github.com>
2024-02-01 18:39:20 -0500

    Cleaned Debug Statement

--- src/main/kotlin/ui/displayed_app.kt
+++ src/main/kotlin/ui/displayed_app.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import utils.*
 import utils.app.ImageFileSelection
 import utils.app.settingsToCSV
 import utils.app.settingsToString
@@ -118,7 +117,6 @@ fun App() {
                                 loadedImageSize = getDim(displayedImage!!)
                             }
                             if (displayedImage != null) {
-                                println(loadedImageSize.width)
                                 displayedNodes = createNodeMask(
                                     generateNodes(NodeGeneratorType.SQUARE)
                                 )
