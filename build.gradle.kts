039b9307fd5b5c42541efa5a9adc2ac066f09e66
aidanmao2005 <aidanmao2005@gmail.com>
2024-03-29 17:08:53 -0400

    Removed Pickles
    = removed: pickles
    
    = change: renamed pkl_parser.kt to data_parser.kt
    
    = removed: Imageloader lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     kotlin("jvm") version "1.9.20"
     id("org.jetbrains.compose") version "1.5.11"
     id("org.jetbrains.dokka") version "1.9.10"
-    id("org.pkl-lang") version("0.25.1")
 }
 
 group = "com.tirana"
@@ -23,9 +22,6 @@ dependencies {
     implementation(compose.ui)
     implementation(compose.material)
     implementation(compose.material3)
-    implementation("io.github.succlz123:compose-imageloader-desktop:0.0.2")
-    implementation("org.pkl-lang:pkl-core:0.25.2")
-    implementation("org.pkl-lang:pkl-config-kotlin:0.25.2")
 }
 
 compose.desktop {
